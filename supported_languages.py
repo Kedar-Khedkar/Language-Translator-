7f2c9b282ba09181652e5a3f46b969292927a1d3
Kedar-Khedkar <70146608+Kedar-Khedkar@users.noreply.github.com>
2021-06-22 19:27:49 +0530

    Update supported_languages.py

--- supported_languages.py
+++ supported_languages.py
@@ -1,13 +1,13 @@
 from ibm_watson import LanguageTranslatorV3
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 
-authenticator = IAMAuthenticator('wBfieaiKFK5oGu5UtBYkaiT3AswMWAibH5lhivnuqZoS')
+authenticator = IAMAuthenticator('Api-key')#Replace with your Api key
 language_translator = LanguageTranslatorV3(
     version='2018-05-01',
     authenticator=authenticator
 )
 
-language_translator.set_service_url('https://api.eu-gb.language-translator.watson.cloud.ibm.com')
+language_translator.set_service_url('URl')#Replace with your URL
 supported_lang_list=[]
 
 languages = language_translator.list_languages().get_result()
@@ -15,4 +15,3 @@ for i in range(76):
     if languages['languages'][i]['supported_as_target']==True & languages['languages'][i]['supported_as_source']==True:
         supported_lang_list.append(languages['languages'][i]['language_name'])
 supported_lang_list.sort()
-print(len(supported_lang_list))
